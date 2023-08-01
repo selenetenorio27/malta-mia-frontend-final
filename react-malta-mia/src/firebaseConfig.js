5acf95086d7f56a197e3d1eeaf74431d0f0a86a6
Sel <selenetenorio27@hotmail.com>
2023-08-01 14:38:32 -0700

    Header y background cerveza

--- react-malta-mia/src/firebaseConfig.js
+++ react-malta-mia/src/firebaseConfig.js
@@ -6,13 +6,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyBczTlKFuVhA9z6QlTZi9hpFF5lt6IbbVA",
+  authDomain: "malta-mia.firebaseapp.com",
+  projectId: "malta-mia",
+  storageBucket: "malta-mia.appspot.com",
+  messagingSenderId: "749765586",
+  appId: "1:749765586:web:8dce7f83919f95cc373e0b",
+  measurementId: "G-0ZE3ETHRGE"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
