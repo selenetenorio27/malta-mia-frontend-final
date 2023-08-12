3f4e5bc1e87186e0bc49d3677e0f915ae626cff2
Sel <selenetenorio27@hotmail.com>
2023-08-11 23:01:25 -0700

    2 images added to our products component

--- react-malta-mia/src/components/NuestrosProductos.js
+++ react-malta-mia/src/components/NuestrosProductos.js
@@ -1,8 +1,17 @@
 import React from 'react';
-
+import './NuestrosProductos.css'; // Importa el archivo de estilos CSS
 
 const NuestrosProductos = () => {
-    // ...
-  };
-  
-  export default NuestrosProductos;
+  return (
+    <div className="nuestros-productos-container">
+      <div className="image-container left">
+        <img src="/assets/cer1.jpg" alt="Imagen 1" />
+      </div>
+      <div className="image-container right">
+        <img src="/assets/cer2.jpg" alt="Imagen 2" />
+      </div>
+    </div>
+  );
+};
+
+export default NuestrosProductos;
