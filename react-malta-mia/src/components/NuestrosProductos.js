5bf465800e90294e24114ee7f0823bb63e9b6b30
Sel <selenetenorio27@hotmail.com>
2023-08-12 02:04:11 -0700

    Background in nuestrosproductos component added

--- react-malta-mia/src/components/NuestrosProductos.js
+++ react-malta-mia/src/components/NuestrosProductos.js
@@ -1,27 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './NuestrosProductos.css'; // Importa el archivo de estilos CSS
+import './NuestrosProductosBackground.css';
 
 const NuestrosProductos = () => {
-    return (
-      <div className="nuestros-productos-container">
-        <div className="imagenes-container-arriba">
-          <div className="image-container-arriba">
-            <img src="/assets/cer1.jpg" alt="Imagen arriba izquierda" />
-          </div>
-          <div className="image-container-arriba">
-            <img src="/assets/cer2.jpg" alt="Imagen arriba derecha" />
-          </div>
-        </div>
-        <div className="imagenes-container">
-          <div className="image-container">
-            <img src="/assets/cer5.jpg" alt="Imagen abajo izquierda" />
-          </div>
-          <div className="image-container">
-            <img src="/assets/cer6.jpg" alt="Imagen abajo derecha" />
-          </div>
-        </div>
-      </div>
-    );
+  
+  useEffect(() => {
+    document.body.classList.add('productos-page');
+  
+    return () => {
+      document.body.classList.remove('productos-page');
+    };
+  }, []);
+
   };
   
   export default NuestrosProductos;
