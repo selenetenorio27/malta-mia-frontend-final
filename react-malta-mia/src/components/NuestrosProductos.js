42a1fde87a2cefc4ae43c65005fbcb589eb80c4c
Sel <selenetenorio27@hotmail.com>
2023-08-12 01:25:47 -0700

    Pictures added to Our Products component

--- react-malta-mia/src/components/NuestrosProductos.js
+++ react-malta-mia/src/components/NuestrosProductos.js
@@ -4,19 +4,24 @@ import './NuestrosProductos.css'; // Importa el archivo de estilos CSS
 const NuestrosProductos = () => {
     return (
       <div className="nuestros-productos-container">
-        <div className="titulo-container">
-          <h2 className="productos-title">Nuestros Productos</h2>
+        <div className="imagenes-container-arriba">
+          <div className="image-container-arriba">
+            <img src="/assets/cer1.jpg" alt="Imagen arriba izquierda" />
+          </div>
+          <div className="image-container-arriba">
+            <img src="/assets/cer2.jpg" alt="Imagen arriba derecha" />
+          </div>
         </div>
         <div className="imagenes-container">
           <div className="image-container">
-            <img src="/assets/cer1.jpg" alt="Imagen izquierda" />
+            <img src="/assets/cer5.jpg" alt="Imagen abajo izquierda" />
           </div>
           <div className="image-container">
-            <img src="/assets/cer2.jpg" alt="Imagen derecha" />
+            <img src="/assets/cer6.jpg" alt="Imagen abajo derecha" />
           </div>
         </div>
       </div>
     );
   };
-
-export default NuestrosProductos;
+  
+  export default NuestrosProductos;
