940d99217d818b7eb291ee9387d54be5c592f55e
Sel <selenetenorio27@hotmail.com>
2023-08-14 17:19:08 -0700

    Bug in Home fixed

--- react-malta-mia/src/App.js
+++ react-malta-mia/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n'; // Archivo de configuración de i18next
@@ -44,6 +44,45 @@ const App= () => {
     setMostrarRecomendacion(false); // Ocultar recomendación al hacer clic en "Encuesta"
   };
   
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;;
+    const maltaMiaCoords = { lat: 19.2845458984375, lng: -99.16448974609375 }; // Coordenadas de Malta Mia
+    const maltaMiaAddress = 'Calz. de Tlalpan 4915-Local 1A, Tlalpan Centro II, Tlalpan, 14000 Ciudad de México, CDMX, México';
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    window.initMap = () => {
+      const mapOptions = {
+        center: maltaMiaCoords,
+        zoom: 14,
+      };
+
+      const map = new window.google.maps.Map(document.getElementById('map'), mapOptions);
+
+      // Agregar el marcador en las coordenadas de Malta Mia
+      const marker = new window.google.maps.Marker({
+        position: maltaMiaCoords,
+        map: map,
+        title: 'Malta Mia',
+        icon: {
+          url: './assets/BeerCup.jpg',
+          scaledSize: new window.google.maps.Size(40, 40),
+        },
+      });
+
+      // Crear el cuadro de dirección
+      const addressBox = new window.google.maps.InfoWindow({
+        content: `<div class="address-box">${maltaMiaAddress}</div>`,
+      });
+
+      // Mostrar el cuadro de dirección al cargar el mapa
+      addressBox.open(map, marker);
+    };
+  }, []);
+
 
   return (
     <Router>
