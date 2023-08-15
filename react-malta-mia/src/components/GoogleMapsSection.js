7291020a199dc7e2a3d949ba04d5da451fafcca8
Sel <selenetenorio27@hotmail.com>
2023-08-15 00:31:04 -0700

    Comments  googlemapssection removed

--- react-malta-mia/src/components/GoogleMapsSection.js
+++ react-malta-mia/src/components/GoogleMapsSection.js
@@ -8,11 +8,6 @@ const GoogleMapsSection = () => {
     const maltaMiaCoords = { lat: 19.2845458984375, lng: -99.16448974609375 }; // Coordenadas de Malta Mia
     const maltaMiaAddress = 'Calz. de Tlalpan 4915-Local 1A, Tlalpan Centro II, Tlalpan, 14000 Ciudad de México, CDMX, México';
 
-    // const script = document.createElement('script');
-    // script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
-    // script.async = true;
-    // script.defer = true;
-    // document.head.appendChild(script);
 
     //window.initMap = () => {
       const mapOptions = {
@@ -57,51 +52,3 @@ const GoogleMapsSection = () => {
 
 export default GoogleMapsSection;
 
-// import React from 'react';
-// import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
-// import './GoogleMapsSection.css';
-
-// const GoogleMapsSection = () => {
-//   const maltaMiaCoords = { lat: 19.2845458984375, lng: -99.16448974609375 };
-//   const maltaMiaAddress =
-//     'Calz. de Tlalpan 4915-Local 1A, Tlalpan Centro II, Tlalpan, 14000 Ciudad de México, CDMX, México';
-
-//   const mapContainerStyle = {
-//     height: '400px',
-//     width: '70%',
-//   };
-
-//   const markerIcon = {
-//     url: './assets/BeerCup.jpg', // URL of your custom icon
-//     scaledSize: new window.google.maps.Size(40, 40), // size of the icon on the map
-//     origin: new window.google.maps.Point(0, 0), // position in the image of the icon to display
-//     anchor: new window.google.maps.Point(20, 20) // position in the marker where the icon's anchor should be. It's the center in this case.
-//   };
-
-//   return (
-//     <div className="map-container">
-//       <LoadScript googleMapsApiKey='AIzaSyAmaglOBMnXfiKkPMGadioN9Q8imFe54Ig'>
-//         <GoogleMap
-//           center={maltaMiaCoords}
-//           zoom={14}
-//           mapContainerStyle={mapContainerStyle}
-//         >
-//           <Marker
-//             position={maltaMiaCoords}
-//             title="Malta Mia"
-//             icon={markerIcon}
-//           >
-//             <InfoWindow>
-//               <div className="address-box">{maltaMiaAddress}</div>
-//             </InfoWindow>
-//           </Marker>
-//         </GoogleMap>
-//       </LoadScript>
-//       <div className="address-container">
-//         <img src="/assets/local.jpg" alt="Malta Mia Address" className="address-image" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default GoogleMapsSection;
