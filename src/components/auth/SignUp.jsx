47f78cb9ca4a2646dbe432728e9cf0baf39e4b7f
Sel <selenetenorio27@hotmail.com>
2023-08-16 15:29:45 -0700

    Sign up CSS added

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -35,23 +35,26 @@ const SignUp = () => {
   };
 
   return (
-    <div className="sign-in-container">
-      <form onSubmit={signUp} className="signup-form">
-        <h1>{t('Sign.up')}</h1>
-        <input
-          type={t('email')}
-          placeholder={t('enter.email')}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type={t('password')}
-          placeholder={t('password')}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">{t('Sign.up')}</button>
-      </form>
+    <div className="sign-up-container">
+        <form onSubmit={signUp} className="signup-form">
+          <h1>{t('Sign.up')}</h1>
+          <input
+            type="email"
+            placeholder={t('enter.email')}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder={t('password')}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">{t('Sign.up')}</button>
+        </form>
+      <div className="sign-up-logo">
+        <img src="/assets/can.png" alt="Logo" />
+      </div>
     </div>
   );
 };
