d1d16755619996c3d6a7bcb108dbdc375f4c8d9b
Sel <selenetenorio27@hotmail.com>
2023-08-16 16:10:52 -0700

    About section version 1 CSS

--- src/components/AcercaDe.js
+++ src/components/AcercaDe.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './AcercaDeBackground.css';
+import './AcercaDe.css';
 import { useTranslation } from 'react-i18next';
 
 const AcercaDe = () => {
   const { t } = useTranslation();
 
+  useEffect(() => {
+    
+    document.body.classList.add('about-page');
+    
+    return () => {
+      document.body.classList.remove('about-page');
+    };
+  }, []);
+
   return (
-    <div>
-      <h2>{t('about')}</h2>
-      <p>{t('welcome')}</p>
-      <p>{t('beerDescription1')}</p>
-      <p>{t('beerDescription2')}</p>
-      <h3>{t('getToKnowUs')}</h3>
-      <p>{t('foundedBy')}</p>
-      <p>{t('passion')}</p>
+    <div className="acercade-container">
+      <h2 className="acercade-title">{t('about')}</h2>
+      <p className="acercade-text">{t('welcome')}</p>
+      <p className="acercade-text">{t('beerDescription1')}</p>
+      <p className="acercade-text">{t('beerDescription2')}</p>
+      
+      <div className="founders-section">
+        <img className="founders-image" src="/assets/cer2.jpg" alt="Founders" />
+        <h3 className="founder-name">{t('foundedBy')}</h3>
+        <p className="founder-description">{t('passion')}</p>
+      </div>
+      
+      <div className="passion-section">
+        <p className="acercade-text">{t('passion')}</p>
+      </div>
     </div>
   );
 };
